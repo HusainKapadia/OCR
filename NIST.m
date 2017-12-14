ee5f895cc3586f46e4bcd42bf733961b04939628
ArthurBrussee <arthur.brussee@gmail.com>
2017-12-14 16:50:14 +0100

    Add learning curve

--- NIST.m
+++ NIST.m
@@ -7,61 +7,44 @@ data = prnist(0:9, 1:1000);
 %TODO: Make a learning curve
 %TODO: Try different PCA sizes / no PCA
 
-for i = 1:10
 
 test_objects = 100;
-data_frac = 0.5;
+data_frac = 0.05;
 
 feat_rep = 'feat_direct';
 
-nn_perf = rec101(data, bpxnc([], [20 10 5], 15000), feat_rep, data_frac, test_objects)
-svc_perf = rec101(data, svc([], proxm('p',3)), feat_rep, data_frac, test_objects)
-linear_perf = rec101(data, perlc([]), feat_rep, data_frac, test_objects)
-tree_perf = rec101(data, treec([]), feat_rep, data_frac, test_objects)
-knnc_perf = rec101(data, knnc([], 5), feat_rep, data_frac, test_objects)
+test_count = 10;
 
-disp("Neural network performance: ")
-disp(nn_perf);
+nn_perf = zeros(test_count);
+svc_perf = zeros(test_count);
+linear_perf = zeros(test_count);
+tree_perf = zeros(test_count);
+knnc_perf = zeros(test_count);
 
-disp("Support vector machine performance: ");
-disp(svc_perf);
+for i = 1:test_count
+    data_frac = i / 100;
+    
+    nn_perf(i) = rec101(data, bpxnc([], [20 10 5], 15000), feat_rep, data_frac, test_objects);
+    svc_perf(i) = rec101(data, svc([], proxm('p',3)), feat_rep, data_frac, test_objects);
+    linear_perf(i) = rec101(data, perlc([]), feat_rep, data_frac, test_objects);
+    tree_perf(i) = rec101(data, treec([]), feat_rep, data_frac, test_objects);
+    knnc_perf(i) = rec101(data, knnc([], 5), feat_rep, data_frac, test_objects);
 
-disp("Linear Perceptron performance: ");
-disp(linear_perf);
+    disp("Neural network performance: ")
+    disp(nn_perf(i));
 
-disp("Binary Tree performance: ");
-disp(tree_perf);
+    disp("Support vector machine performance: ");
+    disp(svc_perf(i));
 
-disp("K-NN performance: ");
-disp(knnc_perf);
+    disp("Linear Perceptron performance: ");
+    disp(linear_perf(i));   
 
+    disp("Binary Tree performance: ");
+    disp(tree_perf(i));
+
+    disp("K-NN performance: ");
+    disp(knnc_perf(i));
 end
 
 
-%prwaitbar off
-% show(data);
-% 
-% 
-% 
-%features = my_rep(data);
-% 
-% 
-% %bbox = 
-
-% 
-% %normalize
-% %normalized = ...; 
-% 
-% net = bpxnc(features, 12);
-% 
-% %TODO: Get 2D features
-% scatterd(features, 'legend');
-% 
-% plotc(net);
-
-
-%for i = 1:100
-%     untrainedNet =  bpxnc([], [10 10]);
-%     eval = nist_eval('my_rep', untrainedNet, 100);
-%     testc(my_rep(data), eval.data(1))
-%end
+plot(svc_perf);
