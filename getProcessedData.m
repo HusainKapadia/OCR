726b80c5ee7aa947d1dfe2178b584b7fe8a22de0
Husain <husainkapadia2811@gmail.com>
2018-01-17 21:53:31 +0100

    changes
    You will now have to pass a 4th argument of pca_dim to the function getprocessedData

--- getProcessedData.m
+++ getProcessedData.m
@@ -2,23 +2,13 @@ function train_struct = getProcessedData(nist, feat_func, data_fraction, pcaDim)
     [nist_dat, ~] = gendat(nist, data_fraction);
     f = str2func(feat_func);
     
-<<<<<<< HEAD
-    fprintf("Got training data. Size: %d %d \n", size(nist_dat,1), size(nist_dat,2));
-    
-=======
-
->>>>>>> b3b0021083c77cc36b06718a838cd2d4ff9780eb
+    fprintf("Got training data. Size: %d %d \n", size(nist_dat,1), size(nist_dat,2));    
     train_data = f(nist_dat);       
     scaling = scalem(train_data, 'c-variance');
     train_data = train_data * scaling;
 
     %TODO: Try different PCA sizes / no PCA
-<<<<<<< HEAD
-    [pca_map, ~] = pcam(train_data, 30); %30 in case of feat_direct
-=======
     [pca_map, ~] = pcam(train_data, pcaDim);
->>>>>>> b3b0021083c77cc36b06718a838cd2d4ff9780eb
-
     fprintf("Got training data. Size: %d %d \n", size(train_data, 1), size(train_data, 2));
     
     
@@ -28,10 +18,6 @@ function train_struct = getProcessedData(nist, feat_func, data_fraction, pcaDim)
     train_struct.data = train_data;
     train_struct.scale = scaling;
     train_struct.pca = pca_map;
-<<<<<<< HEAD
-    
     disp(train_struct);
     disp('Will start training classifier');
-=======
->>>>>>> b3b0021083c77cc36b06718a838cd2d4ff9780eb
 end
